3869db1c9923aa8f44012922081f7a766b40a873
Nicolas Danciu <nixrom1@gmail.com>
2022-04-07 16:01:35 -0400

    finished up the people folder with managers

---
+++ people/Managers.js
@@ -0,0 +1,15 @@
+const Employee = require ('./Employee');
+
+class manager extends Employee {
+    constructor (name, id , email, officenumber) {
+        super (name, id, email);
+        this.officenumber = officenumber;
+    }
+    getRole() {
+        return "Manager";
+    }
+    getOfficeNumber() {
+        return this.officenumber;
+    }
+}
+module.exports = Manager;
