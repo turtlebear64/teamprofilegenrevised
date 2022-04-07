0451c030d4b648ddce5f83984f0def16943f769d
Nicolas Danciu <nixrom1@gmail.com>
2022-04-07 17:42:37 -0400

    fixed file roots

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+const Engineer = require("./people/Engineer");
+const Intern = require("./people/Intern");
+const Manager = require("./people/Manager");
 
 const employees = [];
 
